88df048beaa537ea2567e0aa88633490a09ce68e
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-01-21 11:03:16 +0530

    Time: 2575 ms (5%), Space: 86.7 MB (20.4%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+         int n=nums.length;
+        for(int i=1;i<n;i++){
+            int key=nums[i];
+            int j=i-1;
+            while(j>=0 && nums[j]>key){
+                nums[j+1]=nums[j];
+                j--;
+            }
+            nums[j+1]=key;
+        }
+        
+        return nums;
+
+    }
+}
