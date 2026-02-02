78081cca9881cbcc2ead81d4d0642616f680c2ed
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-02-03 01:37:21 +0530

    Time: 1 ms (34.64%), Space: 43.5 MB (5.29%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] arr = s.split(" ");
+        String word = arr[arr.length - 1];
+        word=word.trim();
+        return word.length();
+    }
+}
