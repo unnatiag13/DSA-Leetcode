603f96a6a4d3fa97b5e75acc56444e425a56e50c
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-02-03 01:40:39 +0530

    Time: 0 ms (100%), Space: 43.1 MB (47.18%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,20 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        String word = arr[arr.length - 1];
-        word=word.trim();
-        return word.length();
+        // String[] arr = s.split(" ");
+        // String word = arr[arr.length - 1];
+        // word=word.trim();
+        // return word.length();
+
+        int len = 0;
+        int i= s.length()-1;
+
+        while(i>=0 && s.charAt(i)==' '){
+            i--;
+        }
+        while(i>=0 && s.charAt(i)!=' '){
+            i--;
+            len++;
+        }
+        return len;
     }
 }
