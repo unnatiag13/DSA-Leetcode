1e7a8e2c3baf179f1d388c46bb38b63c95f2dd3c
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-02-11 21:52:24 +0530

    Time: 1 ms (99.86%), Space: 46.9 MB (63.32%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,13 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer > freq=new HashMap<>();
+        int ans=0;
         for(int i=0;i<nums.length;i++){
-            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
+            ans^=nums[i];
         }
-        int n=0;
-        for(Map.Entry<Integer,Integer> entry:freq.entrySet()){
-            if(entry.getValue()==1) n= entry.getKey();
-        }
-        return n;
+        return ans;
     }
 }
