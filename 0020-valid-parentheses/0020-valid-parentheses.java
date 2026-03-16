395ce9459ef0566f6636e42c838b82f9f2bd2929
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-03-16 16:46:30 +0530

    Time: 4 ms (41.86%), Space: 43.4 MB (37.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
         if(s.length()%2!=0) return false;
+
         boolean isValid = true;
         Stack<Character> st = new Stack<>();
+
         for(char c:s.toCharArray()){
+
             if(c=='(' || c== '[' || c=='{'){
                 st.push(c);
-            }else{
-                if(st.isEmpty()) return false;
-                char top = st.pop();
-                isValid= (top=='(' && c==')') || (top=='{' && c =='}')||( top=='[' && c == ']' );
             }
-            if(isValid ==false) return isValid;
+            else{
+                if(st.isEmpty()) return false;
+
+                char top = st.pop();
+
+               if((top=='(' && c!=')') || 
+                (top=='{' && c !='}')||( top=='[' && c != ']' ))return false;
+            }
         }
-        if(st.isEmpty()) return true;
-        else return false;
+        return st.isEmpty();
     }
 }
