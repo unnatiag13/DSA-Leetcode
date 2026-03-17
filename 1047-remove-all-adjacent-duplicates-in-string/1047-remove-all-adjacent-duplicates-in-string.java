db3f23a55be04d7c66f540ef31311dff771fbe1c
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-03-17 16:26:11 +0530

    Time: 15 ms (72.23%), Space: 46.7 MB (88.89%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,8 +1,7 @@
 class Solution {
     public String removeDuplicates(String s) {
         StringBuilder ans = new StringBuilder();
-        ans.append(s.charAt(0));
-        for(int i=1;i<s.length();i++){
+        for(int i=0;i<s.length();i++){
             if(ans.length()!=0 && s.charAt(i)==ans.charAt(ans.length()-1)){
                 ans.deleteCharAt(ans.length()-1);
             }
