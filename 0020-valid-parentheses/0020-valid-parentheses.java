ff3cffe6a7c524fd08ef59ce28b1768ff23f15f8
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-04-08 16:40:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()%2!=0) return false;
-
-        boolean isValid = true;
-        Stack<Character> st = new Stack<>();
-
-        for(char c:s.toCharArray()){
-
-            if(c=='(' || c== '[' || c=='{'){
-                st.push(c);
-            }
-            else{
-                if(st.isEmpty()) return false;
-
-                char top = st.pop();
-
-               if((top=='(' && c!=')') || 
-                (top=='{' && c !='}')||( top=='[' && c != ']' ))return false;
+       if(s.length()%2!=0) return false;
+       Stack<Character> st = new Stack<>();
+       for(char c:s.toCharArray()){
+        if(c=='(' || c== '{' || c=='['){
+            st.push(c);
+        }
+        else{
+            char d = st.peek();
+            if(d=='(' && c!=')' || d=='{' && c!='}' || d=='[' && c!=']' ){
+                return false;
+            }else{
+                st.pop();
             }
         }
-        return st.isEmpty();
+       }
+       return st.isEmpty();
     }
 }
