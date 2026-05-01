5b75206ab15995f443eb26b9301e68cc9be5642b
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-05-02 01:34:36 +0530

    Time: 0 ms (100%), Space: 44.9 MB (53.9%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head==null || head.next==null) return head;
+       if(head==null || head.next==null) return head;
         ListNode dummy = new ListNode();
         dummy.next=head;
         head=dummy;
