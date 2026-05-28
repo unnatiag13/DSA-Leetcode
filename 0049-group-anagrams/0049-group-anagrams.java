e09917a2fa0a95c1cf54c1083e4d6bef08e8d65d
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-05-29 00:31:27 +0530

    Time: 6 ms (99.42%), Space: 49.4 MB (91.14%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String word:strs){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
