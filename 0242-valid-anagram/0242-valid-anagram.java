43b2bf3118bc5a32d2f4b2d1626287a3f49cd08b
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-05-29 00:07:12 +0530

    Time: 14 ms (27.94%), Space: 46.6 MB (15.47%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray()){
+            if(!map.containsKey(c)) return false;
+            else{
+                map.put(c,map.getOrDefault(c,2)-1);
+            }
+        }
+        for(int v:map.values()){
+            if(v!=0) return false;
+        }
+        return true;
+
+    }
+}
