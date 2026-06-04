02cb2292365aae1199ba482d1ecd2c51e8c68efa
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-06-04 23:53:07 +0530

    Time: 2 ms (98.48%), Space: 81.3 MB (87.46%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int[] arr = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(arr[nums[i]]!=0) return nums[i];
+            arr[nums[i]]++;
+        }
+        return 0;
+    }
+}
