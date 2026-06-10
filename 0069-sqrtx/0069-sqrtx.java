9379453ff225e72673cb0d4702e83f9280a31e86
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-06-11 01:20:57 +0530

    Time: 1 ms (98.04%), Space: 42.4 MB (77.39%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x<2) return x;
+        int left =1, right =x/2;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(mid == x/mid){
+                return mid;
+            }else if(mid < x/mid){
+                left = mid +1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return right;
+    }
+}
