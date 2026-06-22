ca6adcda83305fd2343ce33f4a25478dac4d52b7
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-06-23 03:20:20 +0530

    Time: 3 ms (70.44%), Space: 43.3 MB (79.06%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,22 +1,15 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        Map<Character,Integer> map = new HashMap<>();
-        for(char ch:text.toCharArray()){
-            if(ch=='b' || ch =='a'|| ch=='l'||ch=='o'||ch=='n'){
-                map.put(ch,map.getOrDefault(ch,0)+1);
-            }
-        }
-        if(!map.containsKey('b') || !map.containsKey('a') || 
-            !map.containsKey('l') || !map.containsKey('o') || 
-            !map.containsKey('n')) return 0;
-
-        int[] balloonfreq = {map.get('b'),map.get('a'),map.get('l')/2,map.get('o')/2,map.get('n')};
-
-        int ans=balloonfreq[0];
-        
-        for(int i:balloonfreq){
-            if(i<ans) ans =i;
-        } 
-        return ans;
+       int b=0,a=0,l=0,o=0,n=0;
+       for(char ch:text.toCharArray()){
+            if(ch=='b') b++;
+            if(ch=='a') a++;
+            if(ch=='l') l++;
+            if(ch=='o') o++;
+            if(ch=='n') n++;
+       }
+       l/=2;
+       o/=2;
+       return Math.min(Math.min(b,a),Math.min(l,Math.min(o,n)));
     }
 }
