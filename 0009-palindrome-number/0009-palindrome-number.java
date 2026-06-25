7960c474f8b2deb7673dc3bf89a579da03f27248
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-06-26 04:58:37 +0530

    Time: 4 ms (100%), Space: 46 MB (53.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,14 +3,11 @@ class Solution {
         if(x<0){
             return false;
         }
-        x=Math.abs(x);
-        String s = Integer.toString(x);
-        int st =  0, end =s.length()-1;
-        while(st<end){
-            if(s.charAt(st)!=s.charAt(end)) return false;
-            st++;
-            end--;
+        int num=x,rev=0;
+        while(num!=0){
+            rev = num % 10 + rev*10;
+            num/=10;
         }
-        return true;
+        return x==rev;
     }
 }
