3d625690edcbb77c17aea3f7c78bbb3d53ee1e3d
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-07-04 02:57:14 +0530

    Time: 92 ms (5.26%), Space: 51.2 MB (92.24%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        boolean[][] vis = new boolean[n][m];
+        int islands=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    vis[i][j] = true;
+                    bfs(i,j,grid,vis);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+        
+    }
+    static void bfs(int r,int c,char[][] grid,boolean[][] vis){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{r,c});
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[] rowt = {+1,0,-1,0};
+        int[] colt = {0,+1,0,-1};
+        while(!q.isEmpty()){
+            int row=q.peek()[0];
+            int col=q.poll()[1];
+            for(int k=0;k<4;k++){
+                int i=rowt[k];
+                int j =colt[k];
+                if(row+i>=0&&row+i<n &&col+j>=0&&col+j<m && grid[row+i][col+j]=='1' && !vis[row+i][col+j]){
+                    vis[row+i][col+j] = true;
+                    q.offer(new int[]{row+i,col+j});
+                }
+            }
+        }
+    }
+}
