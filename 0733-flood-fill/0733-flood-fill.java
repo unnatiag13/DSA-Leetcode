b9f2b7d6025c5d57122563449e6c96e33e655191
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-07-03 19:02:13 +0530

    Time: 1 ms (38.75%), Space: 47 MB (11.25%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc]==color) return image;
+
+        int initColor = image[sr][sc];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sr,sc});
+        image[sr][sc] = color;
+        int[] rowt = {+1,0,-1,0};
+        int[] colt = {0,+1,0,-1};
+        while(!q.isEmpty()){
+            int row = q.peek()[0];
+            int col = q.poll()[1];
+            for(int k=0;k<4;k++){
+                int i = rowt[k];
+                int j = colt[k];
+                if(row+i>=0&&row+i<image.length && col+j>=0&&col+j<image[0].length && image[row+i][col+j]==initColor){
+                    q.offer(new int[]{row+i,col+j});
+                    image[row+i][col+j] = color;
+                }
+            }
+        } 
+        return image;
+    }
+}
