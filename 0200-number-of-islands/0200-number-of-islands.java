9ab565348aea4df906b03a1292f1f9a5d0b02b47
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-07-04 03:04:29 +0530

    Time: 3 ms (87.39%), Space: 52.4 MB (33.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,41 +1,29 @@
 class Solution {
+
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        // boolean[][] vis = new boolean[n][m];
-        int islands=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' ){
-                    // vis[i][j] = true;
-                    bfs(i,j,grid);
+        int islands = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
                     islands++;
                 }
             }
         }
         return islands;
-        
     }
-    static void bfs(int r,int c,char[][] grid){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{r,c});
-        int n = grid.length;
-        int m = grid[0].length;
 
-        int[] rowt = {+1,0,-1,0};
-        int[] colt = {0,+1,0,-1};
-        while(!q.isEmpty()){
-            int row=q.peek()[0];
-            int col=q.poll()[1];
-            for(int k=0;k<4;k++){
-                int i=rowt[k];
-                int j =colt[k];
-                if(row+i>=0&&row+i<n &&col+j>=0&&col+j<m && grid[row+i][col+j]=='1'){
-                    // vis[row+i][col+j] = true;
-                    grid[row+i][col+j] = '0';
-                    q.offer(new int[]{row+i,col+j});
-                }
-            }
-        }
+    static void dfs(char[][] grid, int row, int col){
+        if(row < 0 || col < 0 ||
+           row >= grid.length || col >= grid[0].length ||
+           grid[row][col] == '0')
+            return;
+
+        grid[row][col] = '0';
+
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
     }
 }
