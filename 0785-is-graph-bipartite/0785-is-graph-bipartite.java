6b33e34a19870b602e1a851e4b6e2c860409941d
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-07-09 04:15:36 +0530

    Time: 0 ms (100%), Space: 47.5 MB (6.6%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,29 +1,23 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int[] vis = new int[graph.length];
+        int[] color = new int[graph.length];
         // two groups - 1 and 2
         for(int i=0;i<graph.length;i++){
-            if(vis[i]==0){
-                if (bfs(i,graph,vis)==false) return false;
+            if(color[i]==0){
+                if (dfs(i,graph,color,1)==false) return false;
             }
         }
         return true;
     }
-    static boolean bfs(int i,int[][] graph,int[] vis){
-        Queue<int[]> q = new LinkedList<>(); // pos , color
-        q.offer(new int[]{i,1});
-        vis[i] = 1;
-         while(!q.isEmpty()){
-            int parentNode = q.peek()[0];
-            int parentGroup = q.poll()[1];
-            for(int node: graph[parentNode]){
-                int NodeGroup = parentGroup==1?2:1;
-                if(vis[node]==0){
-                    q.offer(new int[]{node,NodeGroup});
-                    vis[node] = NodeGroup;
-                }else if(vis[node]==parentGroup){
-                    return false;
-                }
+    static boolean dfs(int i,int[][] graph,int[] color,int iColor){
+        color[i] = iColor;
+        for(int node:graph[i]){
+            if(color[node]==0){
+                int nodeColor = iColor==1?2:1;
+                if (dfs(node,graph,color,nodeColor)==false) return false;
+            }
+            else if(iColor==color[node]){
+                return false;
             }
         }
         return true;
