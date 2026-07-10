5fdc007792d74cab29c67bd22b009df18b7def78
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-07-11 02:10:44 +0530

    Time: 95 ms (5.52%), Space: 46.6 MB (90.8%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,40 +1,40 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj =new ArrayList<>();
+        int[] indeg = new int[numCourses];
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
         for(int[] courses : prerequisites){
             int u=courses[1], v =courses[0];
             adj.get(u).add(v);
+            indeg[v]++;
         }
 
-        int[] indeg = new int[numCourses];
-        for(int i=0;i<numCourses;i++){
-            for(int j:adj.get(i)){
-                indeg[j]++;
-            }
-        }
+        // for(int i=0;i<numCourses;i++){
+        //     for(int j:adj.get(i)){
+        //         indeg[j]++;
+        //     }
+        // }
         Queue<Integer> q = new LinkedList<>();
         for(int i=0;i<numCourses;i++){
             if(indeg[i]==0) q.offer(i);
         }
 
-        ArrayList<Integer> topo = new ArrayList<>();
+       int[] ans = new int[numCourses];
+       int idx=0;
 
         while(!q.isEmpty()){
             int node = q.poll();
-            topo.add(node);
+            ans[idx]=node;
+            idx++;
             for(int i:adj.get(node)){
                 indeg[i]--;
                 if(indeg[i]==0){
-                    q.offer(i);
+                    q.offer(i); 
                 }
             }
         }
-        if(topo.size()<numCourses) return new int[]{};
-        int[] ans = topo.stream().mapToInt(i -> i).toArray();
-
-        return ans;
+        return idx==numCourses?ans:new int[]{};
     }
 }
