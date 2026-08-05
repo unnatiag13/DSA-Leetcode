5aef54397238f3240aee9fa5a9fc0ffb62c69bd3
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-08-05 23:51:09 +0530

    Time: 62 ms (78.85%), Space: 297.3 MB (6.73%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.java
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -1,48 +1,42 @@
 class Solution {
     public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> rev = new ArrayList<>();
-        Set<Integer> allNodes = new HashSet<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
-            rev.add(new ArrayList<>());
-            allNodes.add(i);
         }
         for(int[] edge:invocations ){
             int u = edge[0];
             int v= edge[1];
             adj.get(u).add(v);
-            rev.get(v).add(u);
         }
-
-        Queue<Integer> q = new LinkedList<>();
         boolean[] vis = new boolean[n];
-        q.offer(k);
         vis[k] = true;
-        Set<Integer> neighbors = new HashSet<>();
-        while(!q.isEmpty()){
-            int node = q.poll();
-            neighbors.add(node);
-            for(int i:adj.get(node)){
-                if(!vis[i]){
-                    q.offer(i);
-                    vis[node] = true;
-                }
+        for(int i:adj.get(k)){
+            if(!vis[i]){
+                dfs(i,adj,vis);
             }
         }
-
-        for(int i:neighbors){
-            for(int j:rev.get(i)){
-                if(!neighbors.contains(j)){
-                    List<Integer> ans = new ArrayList<>();
-                    for(int node:allNodes) ans.add(node);
-                    return ans;
-                }
+        for(int[] edge:invocations){
+            int u= edge[0];
+            int v  = edge[1];
+            if(vis[u]==false && vis[v]==true){
+                List<Integer> ans = new ArrayList<>(); 
+                for(int i=0;i<n;i++) ans.add(i);
+                return ans;
             }
         }
-        allNodes.removeAll(neighbors);
-        List<Integer> ans = new ArrayList<>();
-        for(int i:allNodes) ans.add(i);
+        List<Integer> ans = new ArrayList<>(); 
+        for(int i=0;i<n;i++){ 
+            if(!vis[i]) ans.add(i);
+        }
         return ans;
+    }
+    static void dfs(int node,ArrayList<ArrayList<Integer>> adj,boolean[] vis){
+        vis[node] = true;
+        for(int i:adj.get(node)){
+            if(!vis[i]){
+                dfs(i,adj,vis);
+            }
+        }
     }
 }
