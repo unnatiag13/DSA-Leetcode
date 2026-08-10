793fe518dd227f711778ce4a7b655cb6180f4162
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-08-11 02:52:16 +0530

    Time: 4 ms (45.87%), Space: 54.6 MB (7.8%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,34 @@
 class Solution {
-
     public int numIslands(char[][] grid) {
-        int islands = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid, i, j);
+        int m = grid.length;
+        int n = grid[0].length;
+
+        boolean[][] vis = new boolean[m][n];
+        int islands =0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    dfs(i,j,vis,grid);
                     islands++;
                 }
             }
         }
         return islands;
+        
     }
+    static void dfs(int row, int col, boolean[][] vis,char[][] grid){
+        vis[row][col] = true;
 
-    static void dfs(char[][] grid, int row, int col){
-        if(row < 0 || col < 0 ||
-           row >= grid.length || col >= grid[0].length ||
-           grid[row][col] == '0')
-            return;
+        int[] rowt = {1,0,-1,0};
+        int[] colt = {0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int j = row + rowt[i];
+            int k = col + colt[i];
 
-        grid[row][col] = '0';
-
-        dfs(grid, row + 1, col);
-        dfs(grid, row - 1, col);
-        dfs(grid, row, col + 1);
-        dfs(grid, row, col - 1);
+            if(j<grid.length && j>=0 && k<grid[0].length && k>=0){
+                if(!vis[j][k] && grid[j][k]=='1')
+                    dfs(j,k,vis,grid);
+            }
+        }
     }
 }
