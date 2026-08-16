2588c7690455fdcd44f639fa17772ad9cb70b882
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-08-16 17:59:16 +0530

    Time: 56 ms (28.6%), Space: 70.7 MB (18.22%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        // max length subarray with at most 2 types of fruits
+        Map<Integer,Integer> map = new HashMap<>();
+        int l=0;
+        int maxLen = 0;
+        int distinctFruits=0;
+        for(int r =0;r<fruits.length;r++){
+            map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
+            while(map.size()>2 && l<r){
+                map.put(fruits[l],map.get(fruits[l])-1);
+                if(map.get(fruits[l])==0) map.remove(fruits[l]);
+                l++;
+            }
+            if(map.size()<3) maxLen = Math.max(r-l+1,maxLen);
+        }
+        return maxLen;
+    }
+}
