ef7a31baca3c26d335b6d96e19def54fd88ce633
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-08-16 18:00:49 +0530

    Time: 54 ms (66.49%), Space: 70.8 MB (11.93%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -4,7 +4,6 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
         int l=0;
         int maxLen = 0;
-        int distinctFruits=0;
         for(int r =0;r<fruits.length;r++){
             map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
             while(map.size()>2 && l<r){
@@ -12,7 +11,7 @@ class Solution {
                 if(map.get(fruits[l])==0) map.remove(fruits[l]);
                 l++;
             }
-            if(map.size()<3) maxLen = Math.max(r-l+1,maxLen);
+            maxLen = Math.max(r-l+1,maxLen);
         }
         return maxLen;
     }
