58b8840c31c6f6070dadce4e5a2e56995c724833
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-08-20 10:47:52 +0530

    Time: 176 ms (11.35%), Space: 47.8 MB (5.95%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,16 +14,19 @@
  * }
  */
 class Solution {
-    public int depth(int[] dia ,TreeNode root){
+    public int diameterOfBinaryTree(TreeNode root){
        if(root==null) return 0;
-       int left = depth(dia,root.left);
-       int right = depth(dia,root.right);
-       dia[0] = Math.max(dia[0],left+right);
-       return 1+ Math.max(left,right);
+       int ld = diameterOfBinaryTree(root.left);
+       int rd = diameterOfBinaryTree(root.right);
+       int sd = ht(root.left)+ht(root.right)+2;
+       return Math.max(sd,Math.max(ld,rd));
     }
-    public int diameterOfBinaryTree(TreeNode root) {
-        int[] dia = new int[1];
-        depth(dia,root);
-        return dia[0];
+    public int ht(TreeNode root) {
+        if(root==null) return -1;
+        int lh = ht(root.left);
+        int rh = ht(root.right);
+
+        return Math.max(lh,rh)+1;
+
     }
 }
