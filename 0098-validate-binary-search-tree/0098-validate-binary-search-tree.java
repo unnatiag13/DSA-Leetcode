e2c8e0991d0f82bb6a18e4047ec5ad3e128e1c80
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-08-26 12:18:20 +0530

    Time: 1 ms (16.45%), Space: 47.2 MB (5.02%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,17 +13,30 @@
  *     }
  * }
  */
+
+class BSTPair{
+    boolean isBST = true;
+    Long max = Long.MIN_VALUE;
+    Long min = Long.MAX_VALUE;
+}
 class Solution {
-
     public boolean isValidBST(TreeNode root) {
-        return check(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return ValidBST(root).isBST;
+
     }
-    public boolean check(TreeNode node, long min, long max) {
-        if (node == null) return true;
+    public BSTPair ValidBST(TreeNode root){
+        if(root==null) return new BSTPair();
+        BSTPair lbp = ValidBST(root.left);
+        BSTPair rbp = ValidBST(root.right);
+        BSTPair sbp =new BSTPair();
+        sbp.min = Math.min(lbp.min,Math.min(rbp.min,root.val));
+        sbp.max = Math.max(lbp.max,Math.max(rbp.max,root.val));
 
-        if (node.val <= min || node.val >= max) return false;
-
-        return check(node.left, min, node.val) &&
-               check(node.right, node.val, max);
+        if(lbp.isBST &&rbp.isBST && lbp.max<root.val && rbp.min>root.val){
+            sbp.isBST =true;
+        }else{
+            sbp.isBST = false;
+        }
+        return sbp;
     }
 }
