1d9d97ba26f0ac65846bf7082d14a325a2d64a73
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-09-01 11:40:15 +0530

    Time: 7 ms (6.09%), Space: 44.8 MB (32.57%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        solve(n,0,0,"",ans);
+        return ans;
+    }
+    void solve(int n, int open,int close,String s, List<String> ans){
+        if(s.length()==2*n){
+            ans.add(s);
+            return;
+        }
+        if(open<n){
+            solve(n,open+1,close,s+"(",ans);
+        }
+        if(close<open){
+            solve(n,open,close+1,s+")",ans);
+        }
+    }
+}
