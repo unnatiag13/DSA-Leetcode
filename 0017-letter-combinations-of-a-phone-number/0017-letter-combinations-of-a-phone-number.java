fe0e0ebb228c0107d04321794be3979fba4b9b9c
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-09-01 11:18:16 +0530

    Time: 0 ms (100%), Space: 43 MB (97.56%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -4,19 +4,21 @@ class Solution {
     };
     public List<String> letterCombinations(String digits) {
         List<String> ans = new ArrayList<>();
-        dfs(digits, 0, new StringBuilder(), ans);
+        dfs(digits,0,new StringBuilder(),ans);
         return ans;
     }
-    private void dfs(String digits, int idx, StringBuilder path, List<String> ans) {
-        if (idx == digits.length()) {
+    public void dfs(String digits, int idx,StringBuilder path,List<String> ans){
+        if(digits.length()==idx){
             ans.add(path.toString());
             return;
         }
-        String letters = map[digits.charAt(idx) - '0'];
-        for (char c : letters.toCharArray()) {
+        String letters = map[digits.charAt(idx)-'0'];
+        for(char c:letters.toCharArray()){
             path.append(c);
-            dfs(digits, idx + 1, path, ans);
-            path.deleteCharAt(path.length() - 1);
+            dfs(digits,idx+1,path,ans);
+            path.deleteCharAt(path.length()-1);
         }
     }
+    
+    
 }
