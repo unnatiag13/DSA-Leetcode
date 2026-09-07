ff11464197b6e878df9128062ec9caebffd74ab2
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-09-07 12:26:13 +0530

    Time: 1 ms (99.87%), Space: 46.4 MB (81.19%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -16,7 +16,12 @@ class Solution {
     }
     public void helper(char[][] board ,List<List<String>> allBoards, int col ){
         if(col==board.length){
-            saveBoard(allBoards,board);
+            List<String> temp = new ArrayList<>();
+            for(int i=0;i<col;i++){
+                temp.add(String.valueOf(board[i]));
+            }
+            allBoards.add(temp);
+            return;
         }
         for(int row=0;row<board.length;row++){
             if(!cols[row] && !diagU[row+col] && !diagD[row-col+board.length-1]){
@@ -31,6 +36,10 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> allBoards = new ArrayList<>();
         char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
         cols = new boolean[n];
         diagU = new boolean[2 * n];
         diagD = new boolean[2 * n];
