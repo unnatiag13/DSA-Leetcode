d58512a6e890f65860bf19aed8753d2d67ce4830
Unnati Agarwal <unnatiagarwal1313@gmail.com>
2026-09-24 12:17:43 +0530

    Time: 28 ms (21.26%), Space: 136.9 MB (91.58%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -5,15 +5,9 @@ class Solution {
         map.put(0,-1);
         for(int i=0;i<nums.length;i++){
             sum= (sum + nums[i]) %k;
-            if(sum<0) sum+=k;
-            if(map.containsKey(sum)&& Math.abs(map.get(sum)-i)>=2) return true;
+            if(map.containsKey(sum)&& i-map.get(sum)>=2) return true;
             else if(!map.containsKey(sum)) map.put(sum,i);
         }
-        // for(int key:map.keySet()){
-        //     int n = map.get(key);
-        //     if(n>=2) cnt += (n*(n-1))/2;
-        // }
-        // return cnt;
         return false;
     }
 }
